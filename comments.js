2625783f143f3cbefd9b3f8cce8e49ea101a30c7
Белый Юрий Владимирович <115562406+yurabelyy@users.noreply.github.com>
2023-12-17 00:22:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,26 @@
-// create web server with express
-// express is a framework for nodejs
+// create web server
 const express = require('express');
 const app = express();
-// to parse the body of the request
 const bodyParser = require('body-parser');
-// to work with mongodb
-const MongoClient = require('mongodb').MongoClient;
-// to convert string id to object id
-const ObjectID = require('mongodb').ObjectID;
-// to work with mongodb
-const dbURL = 'mongodb://localhost:27017';
-// to work with mongodb
-const dbName = 'blog';
-// to work with mongodb
-const collectionName = 'comments';
-// to work with mongodb
-const client = new MongoClient(dbURL, { useUnifiedTopology: true });
-// to work with mongodb
-let db;
-// to work with mongodb
-let collection;
-// to work with mongodb
-client.connect((err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('Connected successfully to server');
-  db = client.db(dbName);
-  collection = db.collection(collectionName)});
+const port = 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// set up template engine
+app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static('./public'));
+
+// fire controllers
+const commentsController = require('./controllers/commentsController');
+commentsController(app);
+
+// listen to port
+app.listen(port);
+console.log('You are listening to port 3000');
 
